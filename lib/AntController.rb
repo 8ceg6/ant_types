db829c542bf6d66e34472f468e0dc61f0fa67fb2
8ceg6 <8ceg6@comcast.net>
2020-02-06 18:11:08 -0500

    selection method setup

--- lib/AntController.rb
+++ lib/AntController.rb
@@ -1,33 +1,46 @@
 class Ants::AntController
         
-    
-    def initialize
-        # self.new_ant
-    # binding.pry
-    end
-
     def run
-        self.intro
+        # self.intro
         self.new_ant
         self.list_ants
-       
-          binding.pry
+        self.selection
+        self.ant_description
+        #  binding.pry
     end 
     def intro        
-        puts "please select an ant to learn more."
+        puts "Please select a number to learn more."
+        selection = list_ants
+        puts #{"selection"}
     end 
     
     def new_ant
         ant_array = Ants::Scraper.scraper
         Ants::AntType.create_from_hash(ant_array)
+        # binding.pry
     end 
     
     def list_ants
-        ant_list = Ants::AntType.all.sort{|a,b| a.name <=> b.name}
-        ant_list.each.with_index(1) do |a,i|
+        puts "please select an ant to learn more."
+        @ant_list = Ants::AntType.all.sort{|a,b| a.name <=> b.name}
+        @ant_list.each.with_index(1) do |a,i|
             puts "#{i}. #{a.name}"
+            end
+        # binding.pry
     end
-
-
+    
+    def selection
+        ant_choice = gets.strip
     end 
+    
+    def ant_description
+            details = Ants::AntType.all do |a,b|
+            details.each{|d|d.description}
+            binding.pry
+            # puts "#{a.description}"
+            
+        end
+        
+    end   
+    
 end 
--- lib/ant_type.rb
+++ lib/ant_type.rb
@@ -5,41 +5,28 @@ class Ants::AntType
 
     @@all = []
 
-    def initialize(ant_array)
-        @name = ant_array[:name]
-        @description = ant_array[:description]
-        @size = ant_array[:size]
-        @diet = ant_array[:diet]
-        @habitat = ant_array[:habitat]
+    def initialize(ant_hash)
+        # binding.pry
+        @name = ant_hash[:name]
+        @description = ant_hash[:description]
+        @size = ant_hash[:size]
+        @diet = ant_hash[:diet]
+        @habitat = ant_hash[:habitat]
         @@all << self
-        #  binding.pry
+        # binding.pry
     end 
 
-    def name
-        @name 
-    end 
-
-    def description
-        @description
-    end
-
-    def size
-        @size
-    end 
-
-    def diet 
-        @diet
-    end 
-
-    def habitat 
-        @habitat 
-        binding.pry
-    end 
+    
 
     def self.create_from_hash(ant_array)
         ant_array.each do |a_hash|
             self.new(a_hash)
         end
+    end 
+
+    def self.find_by(input)
+        @@all.detect {|n|n.name == input}
+        binding.pry
     end 
     
     def self.all
